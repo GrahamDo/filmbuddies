8c1938dd407b79ffbab06adb1d5778ba7298c593
Graham Downs <graham>
2012-10-19 12:50:38 +0200

    Created User table

--- db.sql
+++ db.sql
@@ -1,4 +1,38 @@
-delimiter $$
+CREATE DATABASE  IF NOT EXISTS `filmbuddies` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `filmbuddies`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: filmbuddies
+-- ------------------------------------------------------
+-- Server version	5.5.28-log
 
-CREATE DATABASE `filmbuddies` /*!40100 DEFAULT CHARACTER SET latin1 */$$
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `UserId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `DateAddedUtc` datetime NOT NULL,
+  `EmailAddress` varchar(100) NOT NULL,
+  `FullName` varchar(100) NOT NULL,
+  `HashedPassword` varchar(100) NOT NULL,
+  `UtcOffset` smallint(6) NOT NULL DEFAULT '0',
+  `ProfileImageLocation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`UserId`),
+  UNIQUE KEY `EmailAddress_UNIQUE` (`EmailAddress`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
